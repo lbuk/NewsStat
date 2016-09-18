1cf3985580442546298396959402c2bfe0f200ae
L. Bolton <liamthomasbolton@gmail.com>
2016-09-18 01:12:12 +0100

    Update MoreNewsStat_function.R

--- R/MoreNewsStat_function.R
+++ R/MoreNewsStat_function.R
@@ -30,14 +30,14 @@ MoreNewsStat <- function(doc_html) {
   news_text <- paste(news_text, collapse = ' ')
 
   # Define statistical insights - here are some of the most common
-  stat_insights = c("(%)|(percent)|(percentage)|(per cent)|(average)|(bias)|(calculate)|
+  stat_insights = c("(%)|(percent)|(percentage)|(per cent)|(average)|(bias)|(billion)|(calculate)|
                     (causality)|(chi-square)|(correlation)|(data)|(distribution)|
-                    (estimate)|(forecast)|(figure)|(frequency)|(hypothesis)|(inference)|
-                    (regression)|(mean)|(median)|(metadata)|(normal distribution)|
+                    (estimate)|(forecast)|(figure)|(frequency)|(hundred)|(hypothesis)|(inference)|
+                    (regression)|(mean)|(median)|(metadata)|(million)|(normal distribution)|
                     (outlier)|(probability)|(p-value)|(pattern)|
                     (rate)|(skew)|(statistic)|(standard deviation)|(statistical model)|
                     (statistical significance)|(statistically significant)|(survey)|
-                    (trend)|(1)|(2)|(3)|(4)|(5)|(6)|(7)|(8)|(9)|(0)")
+                    (thousand)|(trend)|(trillion)|(1)|(2)|(3)|(4)|(5)|(6)|(7)|(8)|(9)|(0)")
 
     # Separate sentences in text
     sentences <- unlist(strsplit(news_text,split="\\. "))
